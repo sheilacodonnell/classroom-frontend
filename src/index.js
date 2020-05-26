9744383cdafecb551e24fdbb14cc14d377636bf6
Sheila O'Donnell <sheilacodonnell@gmail.com>
2020-05-26 11:32:20 -0400

    Create redux store

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import Provider from 'react-redux';
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const enhancer = compose()
+let reducer = ''
+let store = createStore(reducer, applyMiddleware(thunk))
+
+ReactDOM.render(
+  <Provide store={store}r>
+    <App/>
+  </Provider>,
+  document.getElementById('root'));
